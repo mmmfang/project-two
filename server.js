9a58311b0adfd15dd9723ac79b480e93e29226d0
mmmfang <mae.fang@gmail.com>
2015-10-09 11:09:02 -0400

    added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 	PORT	= process.env.PORT || 5432,
 	server	= express(),
-	MONGOURI = process.env.MONGOLAB_URI,
+	MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
 	dbname	= "some_useful_name",
 	mongoose = require('mongoose');
 
@@ -11,8 +11,8 @@ server.get('/test', function(req,res) {
 	res.end();
 });
 
-mongoose.connect(MONGOURI);
+mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function() {
 	console.log("SERVER IS UP ON PORT:", PORT);
-})
+});
 

3d973953a6f0f1480d506d9946f9087c35c78538
mmmfang <mae.fang@gmail.com>
2015-10-10 21:27:57 -0400

    installed relevant npm and created file structure

--- server.js
+++ server.js
@@ -5,14 +5,35 @@ var express = require('express'),
 	dbname	= "some_useful_name",
 	mongoose = require('mongoose');
 
+var ejs = require('ejs'),
+	bodyParser = require('body-parser'),
+	expressLayouts = require('express-ejs-layouts'),
+	session = require('express-session'),
+	methodOverride = require('method-override'),
+	morgan=require('morgan'),
+	Schema = mongoose.Schema;
+
+
+server.use(express.static("./public"));
+server.use(morgan('dev'));
+server.use(expressLayouts);
+server.use(methodOverride('_method'));
+
+server.set('views', './views');
+server.set('view engine', 'ejs'); 
+
+server.use(bodyParser.urlencoded({
+	extended:true
+}));
 
 server.get('/test', function(req,res) {
 	res.write("Welcome to my amazing app");
 	res.end();
 });
 
-mongoose.connect(MONGOURI + "/" + dbname);
+// mongoose.connect(MONGOURI + "/" + baubleBarForum);
 server.listen(PORT, function() {
 	console.log("SERVER IS UP ON PORT:", PORT);
 });
 
+mongoose.set('debug', false);
